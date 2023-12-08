ad079526d0f421973fc47b661d06ba251dc6cfe1
Minami Itsukaichi <mitukaichi@gmail.com>
2023-12-08 10:15:21 -0800

    add a few more starting files

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,5 @@
+// Important for useMutation: We bring in gql from the @apollo/client library to allow us to parse mutations (and queries) as template literals
+import { gql } from '@apollo/client';
+
+// Important for useMutation: Each mutation we'd like to be able to perform gets exported out of our mutations.js utility
+
---
+++ client/src/utils/queries.js
@@ -0,0 +1,5 @@
+// Important for useQuery: We bring in gql from the @apollo/client library to allow us to parse queries (and mutations) as template literals
+import { gql } from '@apollo/client';
+
+// Important for useQuery: Each query we'd like to be able to perform gets exported out of our queries.js utility
+
