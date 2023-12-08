df7738599fdc56bac112a320e494bfbc0789e19b
Minami Itsukaichi <mitukaichi@gmail.com>
2023-12-08 12:06:42 -0800

    queries file on client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,3 +3,34 @@ import { gql } from '@apollo/client';
 
 // Important for useMutation: Each mutation we'd like to be able to perform gets exported out of our mutations.js utility
 
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
