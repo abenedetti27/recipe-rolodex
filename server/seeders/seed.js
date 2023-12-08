557b73ab853f0a60e15875e8bb1e860d37785d69
Janet Webster <twixmixy@gmail.com>
2023-12-08 14:24:44 -0500

    Update seed.js file to clean and create Recipe, Family, and User data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -7,16 +7,18 @@ const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
   try {
-    await cleanDB('Thought', 'thoughts');
+    await cleanDB('Recipe', 'recipes');
+
+    await cleanDB('Family', 'families');
     
     await cleanDB('User', 'users');
 
     await User.create(userSeeds);
 
     for (let i = 0; i < familySeeds.length; i++) {
-      const { _id, name } = await Thought.create(thoughtSeeds[i]);
+      const { _id, name } = await Family.create(familySeeds[i]);
       const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
+        { username: name },
         {
           $addToSet: {
             thoughts: _id,
