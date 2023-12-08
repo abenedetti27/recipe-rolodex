ba0819fbfe92f7c92c3ed9c050562baaf8130d41
Janet Webster <twixmixy@gmail.com>
2023-12-08 14:23:42 -0500

    Update seed.js file with new model imports and data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,7 +1,8 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const { User, Family, Recipe } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const familySeeds = require('./familySeeds.json');
+const recipeSeeds = require('./recipeSeeds.json');
 const cleanDB = require('./cleanDB');
 
 db.once('open', async () => {
@@ -12,8 +13,8 @@ db.once('open', async () => {
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
+    for (let i = 0; i < familySeeds.length; i++) {
+      const { _id, name } = await Thought.create(thoughtSeeds[i]);
       const user = await User.findOneAndUpdate(
         { username: thoughtAuthor },
         {
