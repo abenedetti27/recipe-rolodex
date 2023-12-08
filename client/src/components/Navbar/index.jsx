cade53426eed6ff524e445f83d11abe8128fae69
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-08 14:06:27 -0800

    Delete header component

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,6 @@
+import { useEffect, useState } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import Logo from './assets/logo/logo.png';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Link as ScrollLink } from 'react-scroll';
+
