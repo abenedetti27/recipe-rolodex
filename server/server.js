a038925e37c67f477c13d206e2480b8b7b02b8ac
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-08 11:18:13 -0800

    Add image upload functionality

--- server/server.js
+++ server/server.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const path = require('path');
-
+const mutler = require('multer');
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+cloudinary.config({
+    cloud_name: '',
+    api_key: '',
+    api_secret: ''
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'my-react-app',
+        format: async (req, file) => 'png', // supports promises as well
+        public_id: (req, file) => 'computed-filename-using-request',
+    },
+});
+
+const upload = mutler({ storage: storage });
 
 app.use(express.static(path.join(__dirname, 'my-react-app/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'my-react-app/build/index.html'));
+});
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    const { secure_url, public_id } = req.file;
+    res.json({ secure_url, public_id });
 });
 
 app.listen(PORT, () => {
