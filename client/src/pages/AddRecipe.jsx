4a5e8633b9fbe77a01863310b1427ad1bdc84ee8
Janet Webster <twixmixy@gmail.com>
2023-12-09 11:58:12 -0500

    Add Recipe page with RecipeForm component

---
+++ client/src/pages/AddRecipe.jsx
@@ -0,0 +1,11 @@
+import RecipeForm from '../components/RecipeForm';
+
+function AddRecipe() {
+    return (
+        <main>
+            <RecipeForm />
+        </main>
+    );
+}
+
+export default RecipeForm;
