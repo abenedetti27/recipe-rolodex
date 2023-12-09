c06bfc81ef33cd8245a8b2f94da3312dc0bac3b6
Minami Itsukaichi <mitukaichi@gmail.com>
2023-12-09 15:02:29 -0800

    move the jwt secret out of .env for now

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,7 +2,7 @@ const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const secret = process.env.JWT_SECRET;
+const secret = "ljzqlU9qw5Wi";
 const expiration = '2h';
 
 module.exports = {
