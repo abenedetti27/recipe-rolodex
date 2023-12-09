d112ebbe0d18bdb215277c275b0581edd9fde673
Evie Hoang <evie.h0325@gmail.com>
2023-12-09 14:37:26 -0800

    Update Home.css and Login.jsx with code.

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import RecipeCard from '../components/RecipeCard';
+import './Home.css';
+
+
+function Home() {
+    return (
+        <main className="cardContainer">
+            <RecipeCard />
+        </main>
+    );
+}
+
+export default Home;
