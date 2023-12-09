0609cc65e3d8daa2f593f429e0d10fc89d05d1d5
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-09 14:22:47 -0800

    Refactor Navbar component

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -22,55 +22,44 @@ export const NavBar = () => {
     setIsLoggedIn(false);
   };
 
-    return (
-        <section className="header" id="home">
+  return (
+    <section className="header" id="home">
         <Router>
-          <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-            <Container>
-            <Navbar.Brand as={ScrollLink} to="home" spy={true} smooth={true} duration={500}>
-                <img src={Logo} alt="logo" style={{ width: '50%', height: 'auto' }}/>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-            <Link to="home" spy={true} smooth={true} duration={500}>
-                  Home
-                </Link>
-                {isLoggedIn ? (
-                  <>
-                    {/* Links for logged-in users */}
-                    <Link to="dashboard" spy={true} smooth={true} duration={500}>
-                      Dashboard
-                    </Link>
-                    <Link to="pinned-recipes" spy={true} smooth={true} duration={500}>
-                      Pinned Recipes
-                    </Link>
-                    <Link to="/" onClick={handleLogout}>
-                      Logout
-                    </Link>
-                  </>
-                ) : (
-                  <>
-                  <Link to="login" spy={true} smooth={true} duration={500}>
-                      Login
-                    </Link>
-                  </>
-                )}
-              </Nav>
-              <span className="navbar-text">
-                {/* ... (existing code) ... */}
-              </span>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </Router>
-    </section>
-  );
-};
-
-            <ScrollLink to="home" spy={true} smooth={true} duration={500}>
+            <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+                <Container>
+                    <Navbar.Brand as={ScrollLink} to="home" spy={true} smooth={true} duration={500}>
+                        <img src={Logo} alt="logo" style={{ width: '50%', height: 'auto' }}/>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav">
+                        <span className="navbar-toggler-icon"></span>
+                    </Navbar.Toggle>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <ScrollLink to="home" spy={true} smooth={true} duration={500}>
+                                Home
+                            </ScrollLink>
+                            {isLoggedIn ? (
+                                <>
+                                    <ScrollLink to="dashboard" spy={true} smooth={true} duration={500}>
+                                        Dashboard
+                                    </ScrollLink>
+                                    <ScrollLink to="pinned-recipes" spy={true} smooth={true} duration={500}>
+                                        Pinned Recipes
+                                    </ScrollLink>
+                                    <Link to="/" onClick={handleLogout}>
+                                        Logout
+                                    </Link>
+                                    </>
+                                ) : (
+                                    <>
+                                        <ScrollLink to="login" spy={true} smooth={true} duration={500}>
+                                            Login
+                                        </ScrollLink>
+                                    </>
+                                )}
+                            </Nav>
+                            <span className="navbar-text">
+                            <ScrollLink to="home" spy={true} smooth={true} duration={500}>
                     {/* what pages do we want to link to from the homepage- when logged out. need to figure out logic for this logged in/ logged out*/}
                                 Home
                             </ScrollLink>
@@ -87,7 +76,6 @@ export const NavBar = () => {
                             <ScrollLink to="link2" spy={true} smooth={true} duration={500}>
                                 logout
                             </ScrollLink>
-                        </Nav>
             <span className="navbar-text">
             {/* do we want to link to any external sites? */}
             <div className="icons">
@@ -96,13 +84,16 @@ export const NavBar = () => {
              <a href=""><img src={navIcon3} alt="" style={{ width: '10%', height: 'auto' }}/></a>   
             </div> 
             {/* <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>  */}
-            </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    </Router>
+  
+
+              </span>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Router>
     </section>
   );
-}
+};
+
 
 export default NavBar;
