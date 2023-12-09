e1c6918902964feb3dd962f16226e3543023e9ab
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-09 13:26:02 -0800

    Add footer component with logo

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,3 +1,11 @@
 import { Container, Row, Col } from "react-bootstrap";
 import logo from "./assets/images/logo.png";
 
+export const Footer = () => {
+    return (
+      <footer className="footer">
+        <Container>
+          <Row className="align-items-center">
+            <Col size={12} sm={6}>
+              <img src={logo} alt="Logo" style={{ width: '40%', height: 'auto' }}/>
+            </Col>
