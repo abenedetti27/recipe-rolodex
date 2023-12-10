9343a1d878bbb513a1ac4ae909daf8fd35e50aaf
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-10 14:39:12 -0800

    Add Footer component to App.jsx

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,28 +1,17 @@
-import { Container, Row, Col } from "react-bootstrap";
-import logo from "./assets/logo/logo.png";
-import navIcon1 from "../assets/logo/navIcon1.png";
+import { Input, initMDB } from "mdb-ui-kit";
+
+initMDB({ Input });
+
 
 export const Footer = () => {
     return (
-      <footer className="footer">
-        <Container>
-          <Row className="align-items-center">
-            <Col size={12} sm={6}>
-              <img src={logo} alt="Logo" style={{ width: '40%', height: 'auto' }}/>
-            </Col>
-            <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
-              <a href="https://github.com/abenedetti27"><img src={navIcon1} alt="Anna" style={{ width: '5%', height: 'auto' }}/></a>
-              <a href="https://github.com/mitsukaichi"><img src={navIcon1} alt="Minami" style={{ width: '5%', height: 'auto' }}/></a>
-              <a href="https://github.com/TwixmixyJanet"><img src={navIcon1} alt="Janet" style={{ width: '5%', height: 'auto' }}/></a>
-              <a href="https://github.com/eviehoang"><img src={navIcon1} alt="Nhi" style={{ width: '5%', height: 'auto' }}/></a>
-            </div>
-            <p>Copyright Team Two 2023. All Rights Reserved</p>
-            </Col>
-            </Row>
-        </Container>
-
-        </footer>
+        <footer class="bg-body-tertiary text-center">
+        <div class="container p-4"></div>
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+          © 2020 Copyright:
+          <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+      </footer>
     )
 }
 
