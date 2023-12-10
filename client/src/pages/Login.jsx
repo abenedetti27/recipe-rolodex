f86973597c22814c0215b89f43acedb79ac30967
Evie Hoang <evie.h0325@gmail.com>
2023-12-10 15:41:46 -0800

    Updated Login page styling and pill navigation

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -67,24 +67,9 @@ initMDB([Tab]);
                     {/* <!-- Password input --> */}
                     <div className="form-outline mb-4">
                         <input type="password" id="loginPassword" className="form-control" />
-                        <label className="form-label" for="loginPassword">Password</label>
+                        <label className="form-label" htmlFor="loginPassword">Password</label>
                     </div>
 
-                    {/* <!-- 2 column grid layout --> */}
-                    <div className="row mb-4">
-                        <div className="col-md-6 d-flex justify-content-center">
-                            {/* <!-- Checkbox --> */}
-                            <div className="form-check mb-3 mb-md-0">
-                                <input className="form-check-input" type="checkbox" value="" id="loginCheck" checked />
-                                <label className="form-check-label" htmlFor="loginCheck"> Remember me </label>
-                            </div>
-                        </div>
-
-                        <div className="col-md-6 d-flex justify-content-center">
-                            {/* <!-- Simple link --> */}
-                            <a href="#!">Forgot password?</a>
-                        </div>
-                    </div>
 
                     {/* <!-- Submit button --> */}
                     <button type="submit" className="btn btn-primary btn-block mb-4">Sign in</button>
@@ -97,27 +82,6 @@ initMDB([Tab]);
             </div>
             <div className="tab-pane fade" id="pills-register" role="tabpanel" aria-labelledby="tab-register">
                 <form>
-                    <div className="text-center mb-3">
-                        <p>Sign up with:</p>
-                        <button type="button" className="btn btn-link btn-floating mx-1">
-                            <i className="fab fa-facebook-f"></i>
-                        </button>
-
-                        <button type="button" className="btn btn-link btn-floating mx-1">
-                            <i className="fab fa-google"></i>
-                        </button>
-
-                        <button type="button" className="btn btn-link btn-floating mx-1">
-                            <i className="fab fa-twitter"></i>
-                        </button>
-
-                        <button type="button" className="btn btn-link btn-floating mx-1">
-                            <i className="fab fa-github"></i>
-                        </button>
-                    </div>
-
-                    <p className="text-center">or:</p>
-
                     {/* <!-- Name input --> */}
                     <div className="form-outline mb-4">
                         <input type="text" id="registerName" className="form-control" />
@@ -147,16 +111,7 @@ initMDB([Tab]);
                         <input type="password" id="registerRepeatPassword" className="form-control" />
                         <label className="form-label" htmlFor="registerRepeatPassword">Repeat password</label>
                     </div>
-
-                    {/* <!-- Checkbox --> */}
-                    <div className="form-check d-flex justify-content-center mb-4">
-                        <input className="form-check-input me-2" type="checkbox" value="" id="registerCheck" checked
-                            aria-describedby="registerCheckHelpText" />
-                        <label className="form-check-label" htmlFor="registerCheck">
-                            I have read and agree to the terms
-                        </label>
-                    </div>
-
+                    
                     {/* <!-- Submit button --> */}
                     <button type="submit" className="btn btn-primary btn-block mb-3">Sign in</button>
                 </form>
