eb21fdf59f15a4336c055d0197a87195d2d7ea44
Janet Webster <twixmixy@gmail.com>
2023-12-10 17:34:10 -0500

    Refactor class names and attributes in Recipe component

--- client/src/pages/Recipe.jsx
+++ client/src/pages/Recipe.jsx
@@ -21,47 +21,47 @@ function Recipe() {
   return (
     <>
     {recipe ? (
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4 mb-4">
-            <div class="card">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 mb-4">
+            <div className="card">
               <img
                 src="../assets/spaghetti.jpg"
                 alt="Spaghetti Bolognese"
-                class="card-img-top"
+                className="card-img-top"
               />
-              <div class="card-body">
-                <h5 class="card-title">{recipe.name}</h5>
-                <p class="card-text">
-                  <div class="row">
-                    <div class="col-md-6">
-                      <span class="field-title">Cooking Time:</span> {recipe.cookingTime} minutes
+              <div className="card-body">
+                <h5 className="card-title">{recipe.name}</h5>
+                <p className="card-text">
+                  <div className="row">
+                    <div className="col-md-6">
+                      <span className="field-title">Cooking Time:</span> {recipe.cookingTime} minutes
                     </div>
-                    <div class="col-md-6">
-                      <span class="field-title">Serving Size:</span> {recipe.servingSize} servings
+                    <div className="col-md-6">
+                      <span className="field-title">Serving Size:</span> {recipe.servingSize} servings
                     </div>
-                    <div class="col-md-6">
-                      <span class="field-title">Cooking Instructions:</span>
+                    <div className="col-md-6">
+                      <span className="field-title">Cooking Instructions:</span>
                       {recipe.instructions}
                     </div>
-                    <div class="col-md-6">
-                      <span class="field-title">Ingredients:</span> {recipe.ingredients}
+                    <div className="col-md-6">
+                      <span className="field-title">Ingredients:</span> {recipe.ingredients}
                     </div>
                   </div>
                 </p>
                 <Link to="">
-                  <span class="badge bg-success">
-                    <i class="bi bi-people-fill"></i> Family: {recipe.family}
+                  <span className="badge bg-success">
+                    <i className="bi bi-people-fill"></i> Family: {recipe.family}
                   </span>
                 </Link>
                 <Link to="">
-                  <span class="badge bg-info">
-                    <i class="bi bi-person-fill"></i> Author: {recipe.author}
+                  <span className="badge bg-info">
+                    <i className="bi bi-person-fill"></i> Author: {recipe.author}
                   </span>
                 </Link>
                 <Link to="">
-                  <span class="badge bg-warning">
-                    <i class="bi bi-pin-angle-fill"></i>
+                  <span className="badge bg-warning">
+                    <i className="bi bi-pin-angle-fill"></i>
                   </span>
                 </Link>
               </div>
@@ -70,8 +70,8 @@ function Recipe() {
           <div>
             {/* If we could find a way for this to return to previous page instead of home, that would be better. */}
             <Link to="/">
-              <span class="badge bg-secondary">
-                <i class="bi bi-arrow-bar-left"></i> Return to Recipes
+              <span className="badge bg-secondary">
+                <i className="bi bi-arrow-bar-left"></i> Return to Recipes
               </span>
             </Link>
           </div>
