2f26a317679cc11269f7596ba6d69abe8607788d
Evie Hoang <evie.h0325@gmail.com>
2023-12-09 21:31:54 -0800

    Refactor RecipeCard component and style

--- client/src/components/RecipeCard/index.jsx
+++ client/src/components/RecipeCard/index.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
-import { initMDB } from 'mdb-ui-kit';
+import { initMDB, Ripple } from 'mdb-ui-kit';
 import { useQuery } from '@apollo/client';
 import { QUERY_ALL_RECIPES } from '../../utils/queries';
+import './style.css';
 
-initMDB();
+initMDB({ Ripple });
+
 
 const RecipeCard = () => {
   const { loading, error, data } = useQuery(QUERY_ALL_RECIPES);
@@ -24,24 +26,24 @@ const RecipeCard = () => {
 
   return (
     <>
-      {recipes.map((recipe) => (
-        <div className="card" id="recipeCard" key={recipe._id}>
-          <div className="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
-            <img src={recipe?.image || ''} className="img-fluid" alt={recipe?.title || ''} />
-            <a href="#!">
-              <div className="mask" style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}></div>
-            </a>
+    {recipes.map((recipe) => (
+          <div className="card m-2" id="recipeCard" key={recipe._id} >
+            <div className="bg-image hover-overlay" data-mdb-ripple-init data-mdb-ripple-color="light">
+              <img src={recipe?.photo || ''} className="img-fluid" alt={recipe?.name || ''} />
+              <a href="#!">
+                <div className="mask" style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}></div>
+              </a>
+            </div>
+            <div className="card-body">
+              <h5 className="card-title mb-1">{recipe?.name || 'No Title'}</h5>
+              <a href="#!" className="btn btn-primary" data-mdb-ripple-init>
+                Button
+              </a>
+            </div>
           </div>
-          <div className="card-body">
-            <h5 className="card-title">{recipe?.title || 'No Title'}</h5>
-            <p className="card-text">{recipe?.description || 'No Description'}</p>
-            <a href="#!" className="btn btn-primary" data-mdb-ripple-init>
-              Button
-            </a>
-          </div>
-        </div>
       ))}
     </>
+      
   );
 };
 
