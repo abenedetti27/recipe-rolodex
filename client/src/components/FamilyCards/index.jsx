f876f0f75337d0b516148074cfd760d14a90a2e2
Minami Itsukaichi <mitukaichi@gmail.com>
2023-12-11 10:00:12 -0800

    add form handler to family card

--- client/src/components/FamilyCards/index.jsx
+++ client/src/components/FamilyCards/index.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { initMDB, Ripple, Modal } from 'mdb-ui-kit';
+import './style.css';
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_FAMILY_RECIPE_PHOTOS } from '../../utils/queries';
 // import { ADD_FAMILY, JOIN_FAMILY } from '../../utils/mutations';
@@ -9,6 +10,18 @@ initMDB({ Ripple, Modal });
 
 const FamilyCard = () => {
     const [families, setFamilies] = useState([]);
+    const [newFamilyName, setNewFamilyName] = useState('');
+    const [searchFamilyId, setSearchFamilyId] = useState('');
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        if ( name === "family_name") {
+            setNewFamilyName(value);
+        } else if ( name === "search-family-by-id"){
+            setSearchFamilyId(value);
+        }  
+    }
 
     // const { loading, error, data } = useQuery(QUERY_FAMILY_RECIPE_PHOTOS, {
     //     variables: { username: Auth.getProfile().authenticatedPerson.username }
@@ -29,7 +42,20 @@ const FamilyCard = () => {
     }, []);
 
     const tabChangeHandler = async (e) => {
-        console.log(e.tagert.id);
+        if (e.target.id == "mdb-tab-search-family") {
+            document.getElementById("mdb-tab-search-family").classList.add("active");
+            document.getElementById("pills-search-family").classList.add("active", "show");
+            document.getElementById("mdb-tab-create-family").classList.remove("active");
+            document.getElementById("pills-create-family").classList.remove("active", "show");
+            setNewFamilyName('');
+            
+        } else if (e.target.id == "mdb-tab-create-family") {
+            document.getElementById("mdb-tab-create-family").classList.add("active");
+            document.getElementById("pills-create-family").classList.add("active", "show");
+            document.getElementById("mdb-tab-search-family").classList.remove("active");
+            document.getElementById("pills-search-family").classList.remove("active", "show");
+            setSearchFamilyId('');
+        }     
     }
 
     return (
@@ -68,24 +94,24 @@ const FamilyCard = () => {
             : <p>You are not a member of any family gruop yet</p> }
         </section>
         <section>
-        <div className="modal fade" tabIndex="-1" id="open-family-modal" display="inherit">
+        <div className="modal fade" tabIndex="-1" id="open-family-modal" aria-modal="true" role="dialog" data-mdb-modal-initialized display="block" >
             <div className="modal-dialog modal-lg">
                 <div className="modal-content">
                 <div className="modal-body p-4">
                     <ul className="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
-                        <li className="nav-item" role="presentation" id="mdb-tab-create-family">
-                            <a className="nav-link"  data-mdb-pill-init="" href="#pills-create-family" role="tab" aria-controls="pills-create-family" aria-selected="true" data-mdb-tab-initialized="true">Create family</a>
+                        <li className="nav-item" role="presentation" onClick={tabChangeHandler}>
+                            <a className="nav-link"  data-mdb-pill-init="" href="#pills-create-family" role="tab" aria-controls="pills-create-family" aria-selected="true" data-mdb-tab-initialized="true" id="mdb-tab-create-family">Create family</a>
                         </li>
-                        <li className="nav-item" role="presentation">
-                            <a className="nav-link active" id="mdb-tab-search-family" data-mdb-pill-init="" href="#pills-search-family" role="tab" aria-controls="pills-search-family" aria-selected="false" data-mdb-tab-initialized="true" tabIndex="-1">Search family</a>
+                        <li className="nav-item" role="presentation" onClick={tabChangeHandler} >
+                            <a className="nav-link active"  data-mdb-pill-init="" href="#pills-search-family" role="tab" aria-controls="pills-search-family" aria-selected="false" data-mdb-tab-initialized="true" tabIndex="-1" id="mdb-tab-search-family">Search family</a>
                         </li>
                     </ul>
                     <div className="tab-content">
                     <div className="tab-pane fade" id="pills-create-family" role="tabpanel" aria-labelledby="mdb-tab-create-family">
                         <form>
                         <div className="form-outline mb-4">
-                            <input type="family_name" id="family_name" className="form-control"/>
-                            <label className="form-label" htmlFor="family_name">Name of the family group</label>
+                            <input type="family_name" id="family_name" name="family_name" className="form-control"  value={newFamilyName}onChange={handleChange}/>
+                            <label className="form-label" htmlFor="family_name">Name of the new family group</label>
                         </div>
                         <button type="submit" className="btn btn-primary btn-block mb-4" data-mdb-ripple-init="">Submit</button>
                         </form>
@@ -93,7 +119,7 @@ const FamilyCard = () => {
                     <div className="tab-pane fade  active show" id="pills-search-family" role="tabpanel" aria-labelledby="mdb-search-family">
                         <form>
                         <div className="form-outline mb-4">
-                            <input type="text" id="search-family-by-id" className="form-control"/>
+                            <input type="text" id="search-family-by-id" name="search-family-by-id" className="form-control" value={searchFamilyId}  onChange={handleChange}/>
                             <label className="form-label" htmlFor="search-family-by-id">Family ID</label>
                         </div>
                         <button type="submit" className="btn btn-primary btn-block mb-1" data-mdb-ripple-init="">Search</button>
