32f8f84828dbe35f909dbe48f30f22d351f0cb9e
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-12 10:43:02 -0800

    env updates

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -2,7 +2,8 @@ const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const secret = "ljzqlU9qw5Wi";
+
+const secret = process.env.JWT_SECRET;
 const expiration = '2h';
 
 module.exports = {
