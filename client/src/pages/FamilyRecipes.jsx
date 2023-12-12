a34b9d877c94f3d7fb642ecad31f8442bd72e62e
Janet Webster <twixmixy@gmail.com>
2023-12-11 19:59:59 -0500

    Add FamilyRecipes page

---
+++ client/src/pages/FamilyRecipes.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import { initMDB, Ripple } from "mdb-ui-kit";
+import { useQuery } from "@apollo/client";
+import { QUERY_FAMILY_RECIPE } from "../utils/queries";
+import "../components/RecipeCard/style.css";
+
+initMDB({ Ripple });
+
+function FamilyRecipes() {
+  const { loading, error, data } = useQuery(QUERY_FAMILY_RECIPE, {
+    variables: { familyId: localStorage.getItem("familyId") },
+  });
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    if (data && data.user && data.user.recipes) {
+      setRecipes(data.user.recipes);
+    }
+  }, [data]);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) {
+    console.error("Error fetching data:", error);
+    return <p>Error: Unable to fetch data</p>;
+  }
+
+  return (
+    <>
+        <h1 className="text-center">Family Recipes</h1>
+      <section className="md-container m-auto" id="dashboard-recipes">
+        <div className="d-flex p-3 flex-wrap" id="cardContainer">
+          {recipes.map((recipe) => (
+            <div className="card mb-4" key={recipe._id}>
+              <div
+                className="bg-image hover-overlay"
+                data-mdb-ripple-init
+                data-mdb-ripple-color="light"
+              >
+                <img
+                  src={recipe?.photo || ""}
+                  className="img-fluid mb-0"
+                  alt={recipe?.name || ""}
+                />
+                <a href="#!">
+                  <div
+                    className="mask"
+                    style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+                  ></div>
+                </a>
+              </div>
+              <div className="card-body p-3">
+                <h5 className="card-title mb-2">
+                  {recipe?.name || "No Title"}
+                </h5>
+                <a href="#!" className="btn btn-primary" data-mdb-ripple-init>
+                  See Recipe
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default FamilyRecipes;
