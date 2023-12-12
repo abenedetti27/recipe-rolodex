1873c0fbd8e9f75848172bf417551aa741554c20
Janet Webster <twixmixy@gmail.com>
2023-12-12 13:46:56 -0500

    Add active class to input element when value is not empty

--- client/src/components/RecipeForm/index.jsx
+++ client/src/components/RecipeForm/index.jsx
@@ -48,6 +48,13 @@ export default function RecipeForm() {
   const handleInputChange = (e) => {
     const { id, value } = e.target;
     setFormData({ ...formData, [id]: value });
+
+    const inputElement = event.target;
+    if (value.trim() !== '') {
+      inputElement.classList.add('active');
+    } else {
+      inputElement.classList.remove('active');
+    }
   };
 
   const handleFamilyChange = (e) => {
