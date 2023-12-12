9e11558f4c250de7b49a7a69d3d792f1acf3afe5
Janet Webster <twixmixy@gmail.com>
2023-12-11 22:07:23 -0500

    main, addrecipe, family recipe, readme

--- client/src/pages/FamilyRecipes.jsx
+++ client/src/pages/FamilyRecipes.jsx
@@ -3,49 +3,34 @@ import { initMDB, Ripple } from "mdb-ui-kit";
 import { useQuery } from "@apollo/client";
 import { QUERY_FAMILY_RECIPE } from "../utils/queries";
 import "../components/RecipeCard/style.css";
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from "react-router-dom";
 
 initMDB({ Ripple });
 
 function FamilyRecipes() {
-    const { familyId } = useParams();
-    console.log(familyId);
-    const { loading, error, data, refetch } = useQuery(QUERY_FAMILY_RECIPE, {
-        variables: { familyId: familyId },
-      });
-
+  const { familyId } = useParams();
+  console.log(familyId);
+  const { loading, error, data } = useQuery(QUERY_FAMILY_RECIPE, {
+    variables: { familyId: familyId },
+  });
+  console.log(data);
   const [recipes, setRecipes] = useState([]);
 
-
   useEffect(() => {
-    console.log(data); 
+    console.log(data);
     if (!loading && data && data.recipes) {
       setRecipes(data.recipes);
     } else if (!loading && error) {
       console.error("Error fetching data:", error);
     }
   }, [data, loading, error]);
-  
-  // Refetch the query after a delay (e.g., 500 ms)
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      refetch();
-    }, 500);
-  
-    return () => clearTimeout(timer);
-  }, [familyId, refetch]);
 
   if (loading) return <p>Loading...</p>;
 
-  if (error) {
-    console.error("Error fetching data:", error);
-    return <p>Error: Unable to fetch data</p>;
-  }
-
   return (
     <>
-        <h1 className="text-center">Family Recipes</h1>
-      <section className="md-container m-auto" id="dashboard-recipes">
+      <h1 className="text-center">Family Recipes</h1>
+      <section className="md-container m-auto" id="family-recipes">
         <div className="d-flex p-3 flex-wrap" id="cardContainer">
           {recipes.map((recipe) => (
             <div className="card mb-4" key={recipe._id}>
@@ -70,9 +55,13 @@ function FamilyRecipes() {
                 <h5 className="card-title mb-2">
                   {recipe?.name || "No Title"}
                 </h5>
-                <a href="#!" className="btn btn-primary" data-mdb-ripple-init>
+                <Link
+                  to={`/recipe/${recipe?._id}`}
+                  className="btn btn-primary"
+                  data-mdb-ripple-init
+                >
                   See Recipe
-                </a>
+                </Link>
               </div>
             </div>
           ))}
