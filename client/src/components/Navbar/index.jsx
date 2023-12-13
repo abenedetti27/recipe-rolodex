dc969c96a35ae2a0bd484b799e6d5d8349eb46cd
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-12-12 16:18:18 -0800

    Refactor Navbar component

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -8,112 +8,103 @@ import App from "../../App.jsx";
 initMDB();
 
 const NavBar = () => {
-  const [scrolled, setScrolled] = useState(false);
+    const [scrolled, setScrolled] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 300);
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 300);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const handleToggle = () => {
+        const navToggle = document.querySelector(".navbar-toggler");
+        const targetID = navToggle.getAttribute("data-mdb-target");
+        const target = document.querySelector(targetID);
+
+        if (target) {
+            target.classList.toggle("show");
+        }
     };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const handleToggle = () => {
-    const navToggle = document.querySelector(".navbar-toggler");
-    const targetID = navToggle.getAttribute("data-mdb-target");
-    const target = document.querySelector(targetID);
-
-    if (target) {
-      target.classList.toggle("show");
-    }
-  };
-  console.log("Auth.loggedIn:", Auth.loggedIn());
-  return (
-    <nav
-      className={`navbar navbar-expand-lg navbar-light bg-body-tertiary ${
-        scrolled ? "scrolled" : ""
-      }`}
-    >
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-mdb-toggle="collapse"
-          data-mdb-target="#navbarTogglerDemo03"
-          aria-controls="navbarTogglerDemo03"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          onClick={handleToggle}
+    console.log("Auth.loggedIn:", Auth.loggedIn());
+    return (
+        <nav
+            className={`navbar navbar-expand-lg navbar-light bg-body-tertiary ${scrolled ? "scrolled" : ""
+                }`}
         >
-          <i className="fas fa-bars"></i>
-        </button>
-        <Link to="/">
-          <img
-            src={Logo}
-            alt="logo"
-            style={{ height: "200px" }}
-            className="d-inline-block align-top"
-          />
-        </Link>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            {/* Other navigation links */}
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/dashboard">
-                Dashboard
-              </Link>
-            </li>
-          </ul>
-          <form className="d-flex input-group w-auto">
-            <input
+            <div className="container-fluid">
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#navbarTogglerDemo03"
+                    aria-controls="navbarTogglerDemo03"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    onClick={handleToggle}
+                >
+                    <i className="fas fa-bars"></i>
+                </button>
+                <Link to="/">
+                    <img
+                        src={Logo}
+                        alt="logo"
+                        style={{ height: "200px" }}
+                        className="d-inline-block align-top"
+                    />
+                </Link>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        {/* Other navigation links */}
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/dashboard">
+                                Dashboard
+                            </Link>
+                        </li>
+                    </ul>
+                    <form className="d-flex input-group w-auto">
+                        {/* <input
               type="search"
               className="form-control"
               placeholder="Search"
               aria-label="Search"
               style={{ width: "300px" }}
-            />
-            <button
-              data-mdb-ripple-init
-              className="btn btn-outline-primary"
-              type="button"
-              data-mdb-ripple-color="dark"
-              style={{ width: "100px" }}
-            >
-              Search
-            </button>
-          </form>
-          {Auth.loggedIn() ? (
-            // If user is authenticated, show logout button
-            <button
-              onClick={Auth.logout}
-              className="btn btn-outline-primary"
-              style={{ width: "100px" }}
-            >
-              Logout
-            </button>
-          ) : (
-            // If user is not authenticated, show login button
-            <Link
-              to="/login"
-              className="btn btn-outline-primary"
-              style={{ width: "100px" }}
-            >
-              Login
-            </Link>
-          )}
-        </div>
-      </div>
-    </nav>
-  );
+            /> */}
+                        <Link to="/search" className="btn btn-outline-primary" style={{ width: '100px', height: '36px' }}>Search</Link>
+                    </form>
+                    {Auth.loggedIn() ? (
+                        // If user is authenticated, show logout button
+                        <button
+                            onClick={Auth.logout}
+                            className="btn btn-outline-primary"
+                            style={{ width: "100px" }}
+                        >
+                            Logout
+                        </button>
+                    ) : (
+                        // If user is not authenticated, show login button
+                        <Link
+                            to="/login"
+                            className="btn btn-outline-primary"
+                            style={{ width: "100px" }}
+                        >
+                            Login
+                        </Link>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default NavBar;
